372d76f6d5424cbb5baa9fe0e3fd782930001b46
just.natalka <dzhusnatalka@gmail.com>
2023-08-16 11:43:01 +0300

    Done

---
+++ Lesson1/main.js
@@ -0,0 +1,54 @@
+let a1 = 'hello'
+console.log(a1);
+
+let a2 = 'owu'
+console.log(a2);
+
+let a3 = 'com'
+console.log(a3);
+
+let n1 = 1
+console.log(n1)
+
+let n2 = 10
+console.log(n2)
+
+let n3 = -999
+console.log(n3)
+
+let n4 = 123
+console.log(n4)
+
+let n5 = 3.14
+console.log(n5)
+
+let n6 = 2.7
+console.log(n6)
+
+let n7 = 16
+console.log(n7)
+
+let b1 = 20
+let b2 = 40
+let bRes1 = b2 > b1;
+console.log(bRes1);
+
+let b3 = 50
+let b4 = 40
+let bRes2 = b2 < b1;
+console.log(bRes2);
+
+let a10 = 'Nataliia';
+let a11 = 'Stepanivna';
+let a12 = 'Dzhus';
+let result =`${a10} ${a11} ${a12}`;
+console.log(result);
+
+
+let a= 100;
+let b = '100'
+let c = true;
+console.log (a+b+c)
+
+
+
