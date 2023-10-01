5d59f0e51d17d6b7a3826edae4ab0c2d28a0cf3e
just.natalka <dzhusnatalka@gmail.com>
2023-10-02 00:38:10 +0300

    homework12 upd

--- Lesson12/main12.js
+++ Lesson12/main12.js
@@ -1,3 +1,9 @@
+// зробити файл users.html
+// з ендпоінту http://jsonplaceholder.typicode.com/users отримати всіх користувачів
+// вивести їх id + name списком та додати посилання з href = user-details.html?id=XXX (замість ХХХ - айді юзера)
+// при кліку на посилання перехід на відповідну сторінку, на якій буде
+// вся інформація про користувача (всі 15 полів) отримана через
+// додатковий запит (https://jsonplaceholder.typicode.com/users/XXX   ХХХ - айді користувача)
 fetch('http://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(allUsers => {
